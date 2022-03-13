7c13911c88b00dea02f89e4f3a85a530f85a70b1
William Adams <Wiladams@users.noreply.github.com>
2022-03-12 21:53:30 -0800

    doing cleanup

--- tests/texmap/texmap.cpp
+++ tests/texmap/texmap.cpp
@@ -10,7 +10,7 @@ void setup()
         RainbowSampler s(gamma); 
         for (int x = 0; x < canvasWidth; x++) {
             double u = maths::Map(x, 0, canvasWidth - 1, 0, 1);
-            auto c = s.getValue(u);
+            auto c = s.getValue(u, {});
             gAppSurface->set(x, y, c);
         }
     }
